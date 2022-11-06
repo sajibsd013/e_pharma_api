839157555f84ade3079f2d893c845e0eefb2454a
sajibsd013 <sajibsd013@gmail.com>
2022-11-06 21:03:26 +0600

    update

--- partners/models.py
+++ partners/models.py
@@ -18,38 +18,51 @@ GENDER = (
 # lets us explicitly set upload path and filename
 
 
-def upload_to_doctor(instance, filename):
-    return 'images/doctor/{filename}'.format(filename=filename)
+def upload_to_dmp_doctor(instance, filename):
+    return 'images/dmp_doctor/{filename}'.format(filename=filename)
 
 
 class DMF_Doctor(models.Model):
     name = models.CharField(max_length=120)
     mobile = models.CharField(max_length=120, unique=True)
-    nid = models.CharField(max_length=120, unique=True, null=True)
     qualicifacions = models.CharField(max_length=120)
     specialty = models.CharField(max_length=120)
-    bmdc_regi_no = models.CharField(max_length=120)
+    bmdc_regi_no = models.CharField(max_length=120, unique=True)
     type = models.CharField(max_length=120)
-    working_area = models.CharField(
+    city = models.CharField(
         max_length=120, null=True, blank=True)
-    working_days = models.CharField(max_length=120)
-    working_times = models.CharField(max_length=120)
-    working_times = models.CharField(max_length=120)
+    working_area = models.CharField(
+        max_length=500, null=True, blank=True, )
+    working_days_home_call = models.CharField(
+        max_length=120, null=True, blank=True)
+    working_days_video_call = models.CharField(
+        max_length=120, null=True, blank=True)
+    working_times_chamber = models.CharField(
+        max_length=120, null=True, blank=True)
+    working_times_home_call = models.CharField(
+        max_length=120, null=True, blank=True)
+    working_times_video_call = models.CharField(
+        max_length=120, null=True, blank=True)
+    gender = models.CharField(
+        max_length=120, null=True, blank=True)
+
+    fee_video_call = models.CharField(
+        max_length=120, null=True, blank=True)
     payment_method = models.CharField(max_length=120, null=True)
     payment_number = models.CharField(max_length=120, null=True)
+    whatsapp_number = models.CharField(max_length=120, null=True, blank=True)
     status = models.CharField(
         max_length=120, choices=STATUS_CHOICES, default="pending", )
     image_url = models.ImageField(
-        upload_to=upload_to_doctor, blank=True, null=True)
+        upload_to=upload_to_dmp_doctor, null=True)
     experience = models.TextField()
-
     institution_or_chamber_address = models.TextField(
-        null=True, blank=True)
+        null=True, blank=True, )
     short_description = models.TextField()
     created_date = models.DateTimeField(default=now, editable=False)
 
     class Meta:
-        verbose_name_plural = "Home call/Video Call DMF/CCP Doctors List"
+        verbose_name_plural = "DMF/CCP Doctors List"
 
 
 def upload_to_doctor(instance, filename):
@@ -59,21 +72,35 @@ def upload_to_doctor(instance, filename):
 class Doctor(models.Model):
     name = models.CharField(max_length=120)
     mobile = models.CharField(max_length=120, unique=True)
-    nid = models.CharField(max_length=120, unique=True, null=True)
     qualicifacions = models.CharField(max_length=120)
     specialty = models.CharField(max_length=120)
-    bmdc_regi_no = models.CharField(max_length=120)
+    bmdc_regi_no = models.CharField(max_length=120, unique=True)
     type = models.CharField(max_length=120)
     city = models.CharField(
-        max_length=120, null=True, blank=True, )
+        max_length=120, null=True, blank=True)
     working_area = models.CharField(
         max_length=500, null=True, blank=True, )
-    working_days = models.CharField(max_length=120)
-    working_times = models.CharField(max_length=120)
+    working_days_chamber = models.CharField(
+        max_length=120, null=True, blank=True)
+    working_days_home_call = models.CharField(
+        max_length=120, null=True, blank=True)
+    working_days_video_call = models.CharField(
+        max_length=120, null=True, blank=True)
+    working_times_chamber = models.CharField(
+        max_length=120, null=True, blank=True)
+    working_times_home_call = models.CharField(
+        max_length=120, null=True, blank=True)
+    working_times_video_call = models.CharField(
+        max_length=120, null=True, blank=True)
     gender = models.CharField(
-        max_length=120, choices=GENDER, null=True)
+        max_length=120, null=True, blank=True)
+    fee_chamber = models.CharField(
+        max_length=120, null=True, blank=True)
+    fee_video_call = models.CharField(
+        max_length=120, null=True, blank=True)
     payment_method = models.CharField(max_length=120, null=True)
     payment_number = models.CharField(max_length=120, null=True)
+    whatsapp_number = models.CharField(max_length=120, null=True, blank=True)
     status = models.CharField(
         max_length=120, choices=STATUS_CHOICES, default="pending", )
     image_url = models.ImageField(
@@ -85,7 +112,7 @@ class Doctor(models.Model):
     created_date = models.DateTimeField(default=now, editable=False)
 
     class Meta:
-        verbose_name_plural = "Home call/Video Call Doctors List"
+        verbose_name_plural = "Doctors List"
 
 
 def upload_to_partner(instance, filename):
@@ -100,12 +127,16 @@ class Partner(models.Model):
     service_provider_mobile = models.CharField(max_length=120, null=True)
     nid = models.CharField(max_length=120, unique=True, null=True)
     tred_licence = models.CharField(max_length=120, unique=True, null=True)
+    city = models.CharField(
+        max_length=120, null=True, blank=True)
     working_area = models.CharField(max_length=120)
-    status = models.CharField(
-        max_length=120, choices=STATUS_CHOICES, default="pending")
+    payment_method = models.CharField(max_length=120, null=True)
+    payment_number = models.CharField(max_length=120, null=True)
     image_url = models.ImageField(
         upload_to=upload_to_partner, blank=True, null=True)
     short_description = models.TextField()
+    status = models.CharField(
+        max_length=120, choices=STATUS_CHOICES, default="pending")
     created_date = models.DateTimeField(default=now, editable=False)
 
     class Meta:
@@ -121,7 +152,11 @@ class CareGiver(models.Model):
     name = models.CharField(max_length=120)
     mobile = models.CharField(max_length=120, unique=True)
     nid = models.CharField(max_length=120, unique=True, null=True)
+    city = models.CharField(
+        max_length=120, null=True, blank=True)
     working_area = models.CharField(max_length=120)
+    payment_method = models.CharField(max_length=120, null=True)
+    payment_number = models.CharField(max_length=120, null=True)
     working_days = models.CharField(max_length=120)
     qualicifacions = models.CharField(max_length=120, null=True)
     status = models.CharField(
@@ -150,7 +185,11 @@ class Nurse(models.Model):
     mobile = models.CharField(max_length=120, unique=True)
     nid = models.CharField(max_length=120, unique=True, null=True)
     qualicifacions = models.CharField(max_length=120)
+    city = models.CharField(
+        max_length=120, null=True, blank=True)
     working_area = models.CharField(max_length=120)
+    payment_method = models.CharField(max_length=120, null=True)
+    payment_number = models.CharField(max_length=120, null=True)
     working_days = models.CharField(max_length=120)
     working_times = models.CharField(max_length=120)
     institution = models.TextField(
@@ -181,7 +220,11 @@ class Physiotherapist(models.Model):
     nid = models.CharField(max_length=120, unique=True, null=True)
     qualicifacions = models.CharField(max_length=120)
     institution = models.CharField(max_length=120, null=True)
+    city = models.CharField(
+        max_length=120, null=True, blank=True)
     working_area = models.CharField(max_length=120)
+    payment_method = models.CharField(max_length=120, null=True)
+    payment_number = models.CharField(max_length=120, null=True)
     working_days = models.CharField(max_length=120)
     working_times = models.CharField(max_length=120)
     status = models.CharField(
